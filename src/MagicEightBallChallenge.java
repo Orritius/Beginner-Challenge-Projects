8ad9e01ba29d7e3e4331ada2a31e9a903ff97262
Matt <72013741+Orritius@users.noreply.github.com>
2020-10-01 12:27:36 +0100

    Update with basic GUI for MagicEightBallChallenge

--- src/MagicEightBallChallenge.java
+++ src/MagicEightBallChallenge.java
@@ -1,38 +1,111 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
-public class MagicEightBallChallenge {
+public class MagicEightBallChallenge extends JFrame{
+    // Component declaration
+    private JPanel mainPanel;
+    private JPanel topPanel;
+    private JLabel instructionLabel;
+    private JLabel eightBallAnswerLabel;
+    private JTextField userQuestionField;
+    private JButton askButton;
+    private JButton clearButton;
+    private JButton playAgainButton;
+    private JButton quitButton;
 
+    private static final String[] responses = {"Yes", "No", "Maybe", "Perhaps", "I am unsure",
+            "Wait another day, then ask again", "Cannot predict now", "Most Likely", "Definitely", "You may rely on it",
+            "Almost certainly", "Definitely", "Never", "Definitely not"};
 
-    public void askTheEightBall() {
-        String[] responses = {"Yes", "No", "Maybe", "Perhaps", "I am unsure", "Wait another day, then ask again",
-                "Cannot predict now", "Most Likely", "Definitely", "You may rely on it", "Almost certainly", "Definitely",
-                "Never", "Definitely not"};
-        boolean playAgain = true;
+    public MagicEightBallChallenge(){
+        initComponents();
+    }
 
-        while (playAgain) {
-            System.out.println("Please enter your question for the eight ball: ");
-            Scanner sc = new Scanner(System.in);
-            sc.nextLine();
+    // Set up all components of the GUI and add button action listeners
+    private void initComponents(){
+        setTitle("Magic Eight Ball challenge");
+        mainPanel = new JPanel();
+        topPanel = new JPanel();
+        instructionLabel = new JLabel("Ask the eight ball a question!");
+        instructionLabel.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        eightBallAnswerLabel = new JLabel();
+        eightBallAnswerLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        userQuestionField = new JTextField();
+        userQuestionField.setColumns(25);
+        askButton = new JButton("Ask");
+        clearButton = new JButton("Clear");
+        playAgainButton = new JButton("Play Again");
+        quitButton = new JButton("Quit");
 
-            System.out.println("Thinking...");
+        getContentPane().add(BorderLayout.NORTH, topPanel);
+        topPanel.add(instructionLabel);
+        getContentPane().add(mainPanel);
+
+        mainPanel.add(userQuestionField);
+        mainPanel.add(askButton);
+        mainPanel.add(clearButton);
+        mainPanel.add(playAgainButton);
+        mainPanel.add(quitButton);
+        mainPanel.add(eightBallAnswerLabel);
+
+        askButton.addActionListener(e -> askButtonActionPerformed());
+
+        clearButton.addActionListener(e -> clearButtonActionPerformed());
+
+        playAgainButton.addActionListener(e -> playAgainButtonActionPerformed());
+
+        quitButton.addActionListener(e -> quitButtonActionPerformed());
+
+        go();
+    }
+
+    // Simulate thinking of eight ball by starting new thread with sleep function
+    private void askTheEightBall() {
+        eightBallAnswerLabel.setText("Thinking...");
+
+        MyThread thread = new MyThread();
+        thread.start();
+    }
+
+    // Display GUI
+    private void go(){
+        setSize(300, 200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+
+    // Simulate computer thinking before choosing a response
+    private class MyThread extends Thread{
+        @Override
+        public void run(){
             try {
-                TimeUnit.SECONDS.sleep(2);
+                TimeUnit.SECONDS.sleep(3);
             } catch (InterruptedException ie) {
                 ie.printStackTrace();
             }
             int response = (int) (Math.random() * responses.length);
 
-            System.out.println(responses[response]);
-
-            System.out.println("Would you like to ask another question? Y/N");
-            char userResponse = sc.nextLine().charAt(0);
-
-            if (userResponse == 'N' || userResponse == 'n') {
-                sc.close();
-                playAgain = false;
-            }
-
+            eightBallAnswerLabel.setText(responses[response]);
         }
     }
+    
+    // Action Listeners methods
+    private void askButtonActionPerformed(){
+        askTheEightBall();
+    }
+
+    private void clearButtonActionPerformed(){
+        userQuestionField.setText("");
+    }
+
+    private void playAgainButtonActionPerformed(){
+        askTheEightBall();
+    }
+
+    private void quitButtonActionPerformed(){
+        dispose();
+    }
+    
 }
