2caa361a8e48272f96f50476ba32493e8028d296
Matt <72013741+Orritius@users.noreply.github.com>
2021-01-19 12:27:00 +0000

    Update CoinCalculator.java
    Completed solution. We need to print out all coins (even if they do not factor into the change given). Otherwise, we could use certain conditions to check if change was required first (as in previous solutions)

--- src/CoinCalculator.java
+++ src/CoinCalculator.java
@@ -19,63 +19,53 @@ public abstract class CoinCalculator {
     public static void calculateChange(BigDecimal anAmount){
         // Variable to keep track of how much change is still required to be given
         BigDecimal amountLeft = anAmount;
-        // Variables to track the amount of each coin that needs to be given
-        //TODO consider that this could be put into a collection?
-        BigDecimal numOfTwoPoundCoins = new BigDecimal("0");
-        BigDecimal numOfOnePoundCoins = new BigDecimal("0");
-        BigDecimal numOfFiftyPenceCoins = new BigDecimal("0");
-        BigDecimal numOfTwentyPenceCoins = new BigDecimal("0");
-        BigDecimal numOfTenPenceCoins = new BigDecimal("0");
-        BigDecimal numOfFivePenceCoins = new BigDecimal("0");
-        BigDecimal numOfTwoPenceCoins = new BigDecimal("0");
-        BigDecimal numOfOnePenceCoins = new BigDecimal("0");
 
-        for (int i = 0; i < 8; i++){
-            if (!amountLeft.divide(new BigDecimal(UNITED_KINGDOM_COINS[i]), 0 , RoundingMode.FLOOR)
-            .equals(new BigDecimal("0"))){
-                switch (i){
-                    case 0:
-                        numOfTwoPoundCoins = amountLeft.divide(new BigDecimal("2.00"),0, RoundingMode.DOWN);
-                        amountLeft = amountLeft.subtract(numOfTwoPoundCoins.multiply(new BigDecimal("2.00")));
-                        System.out.println("Number of £2 coins needed: "
-                                + numOfTwoPoundCoins);
-                    case 1:
-                        numOfOnePoundCoins = amountLeft.divide(new BigDecimal("1.00"),0, RoundingMode.DOWN);
-                        amountLeft = amountLeft.subtract(numOfOnePoundCoins.multiply(new BigDecimal("1.00")));
-                        System.out.println("Number of £1 coins needed: "
-                                + numOfOnePoundCoins);
-                    case 2:
-                        numOfFiftyPenceCoins = amountLeft.divide(new BigDecimal("0.50"),0, RoundingMode.DOWN);
-                        amountLeft = amountLeft.subtract(numOfFiftyPenceCoins.multiply(new BigDecimal("0.50")));
-                        System.out.println("Number of 50p coins needed: "
-                                + numOfFiftyPenceCoins);
-                    case 3:
-                        numOfTwentyPenceCoins = amountLeft.divide(new BigDecimal("0.20"),0, RoundingMode.DOWN);
-                        amountLeft = amountLeft.subtract(numOfTwentyPenceCoins.multiply(new BigDecimal("0.20")));
-                        System.out.println("Number of 20p coins needed: "
-                                + numOfTwentyPenceCoins);
-                    case 4:
-                        numOfTenPenceCoins = amountLeft.divide(new BigDecimal("0.10"),0, RoundingMode.DOWN);
-                        amountLeft = amountLeft.subtract(numOfTenPenceCoins.multiply(new BigDecimal("0.10")));
-                        System.out.println("Number of 10p coins needed: "
-                                + numOfTenPenceCoins);
-                    case 5:
-                        numOfFivePenceCoins = amountLeft.divide(new BigDecimal("0.05"),0, RoundingMode.DOWN);
-                        amountLeft = amountLeft.subtract(numOfFivePenceCoins.multiply(new BigDecimal("0.05")));
-                        System.out.println("Number of 5p coins needed: "
-                                + numOfFivePenceCoins);
-                    case 6:
-                        numOfTwoPenceCoins = amountLeft.divide(new BigDecimal("0.02"),0, RoundingMode.DOWN);
-                        amountLeft = amountLeft.subtract(numOfTwoPenceCoins.multiply(new BigDecimal("0.02")));
-                        System.out.println("Number of 2p coins needed: "
-                                + numOfTwoPenceCoins);
-                    case 7:
-                        numOfOnePenceCoins = amountLeft.divide(new BigDecimal("0.01"),0, RoundingMode.DOWN);
-                        amountLeft = amountLeft.subtract(numOfOnePenceCoins.multiply(new BigDecimal("0.01")));
-                        System.out.println("Number of 1p coins needed: "
-                                + numOfOnePenceCoins);
-                }
-            }
-        }
+        // Array holding each number of coin needed (ranging from £2 to 1p)
+        BigDecimal[] numOfCoins = {new BigDecimal("0"), new BigDecimal("0"), new BigDecimal("0"),
+                new BigDecimal("0"), new BigDecimal("0"), new BigDecimal("0"), new BigDecimal("0"),
+                new BigDecimal("0")};
+
+        /* Because we have to print out coins all coins (even if they are not needed in the change, we don't need
+        to do any form of testing first (as in previous solution iterations)
+         */
+
+        numOfCoins[0] = amountLeft.divide(new BigDecimal("2.00"),0, RoundingMode.DOWN);
+        amountLeft = amountLeft.subtract(numOfCoins[0].multiply(new BigDecimal("2.00")));
+        System.out.println("Number of £2 coins needed: "
+                                + numOfCoins[0]);
+
+        numOfCoins[1] = amountLeft.divide(new BigDecimal("1.00"),0, RoundingMode.DOWN);
+        amountLeft = amountLeft.subtract(numOfCoins[1].multiply(new BigDecimal("1.00")));
+        System.out.println("Number of £1 coins needed: "
+                + numOfCoins[1]);
+
+        numOfCoins[2] = amountLeft.divide(new BigDecimal("0.50"),0, RoundingMode.DOWN);
+        amountLeft = amountLeft.subtract(numOfCoins[2].multiply(new BigDecimal("0.50")));
+        System.out.println("Number of 50p coins needed: "
+                + numOfCoins[2]);
+
+        numOfCoins[3] = amountLeft.divide(new BigDecimal("0.20"),0, RoundingMode.DOWN);
+        amountLeft = amountLeft.subtract(numOfCoins[3].multiply(new BigDecimal("0.20")));
+        System.out.println("Number of 20p coins needed: "
+                + numOfCoins[3]);
+
+        numOfCoins[4] = amountLeft.divide(new BigDecimal("0.10"),0, RoundingMode.DOWN);
+        amountLeft = amountLeft.subtract(numOfCoins[4].multiply(new BigDecimal("0.10")));
+        System.out.println("Number of 10p coins needed: "
+                + numOfCoins[4]);
+
+        numOfCoins[5] = amountLeft.divide(new BigDecimal("0.05"),0, RoundingMode.DOWN);
+        amountLeft = amountLeft.subtract(numOfCoins[5].multiply(new BigDecimal("0.05")));
+        System.out.println("Number of 5p coins needed: "
+                + numOfCoins[5]);
+
+        numOfCoins[6] = amountLeft.divide(new BigDecimal("0.02"),0, RoundingMode.DOWN);
+        amountLeft = amountLeft.subtract(numOfCoins[6].multiply(new BigDecimal("0.02")));
+        System.out.println("Number of 2p coins needed: "
+                + numOfCoins[6]);
+
+        numOfCoins[7] = amountLeft.divide(new BigDecimal("0.01"),0, RoundingMode.DOWN);
+        System.out.println("Number of 1p coins needed: "
+                + numOfCoins[7]);
     }
 }
