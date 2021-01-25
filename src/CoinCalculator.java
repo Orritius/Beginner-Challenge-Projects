a99ea9d72054d6e4c4ce1e7504cc69d95b94c09d
Matt <72013741+Orritius@users.noreply.github.com>
2021-01-25 18:59:05 +0000

    Update to calculate change

--- src/CoinCalculator.java
+++ src/CoinCalculator.java
@@ -5,9 +5,8 @@ public abstract class CoinCalculator {
     public static final String[] UNITED_KINGDOM_COINS = {("2.00"), ("1.00"), ("0.50"), ("0.20"), ("0.10"),
             ("0.05"), ("0.02"), ("0.01")};
 
-    //TODO subgoal - calculate how much change is needed from a transaction, instead of being given the amount of change
     //TODO subgoal - Loop the program
-    /*
+    /**
     The challenge uses US currency, I shall be using UK currency. Program assumes that we're prioritising giving
     the customer the highest amount of change. I.e. we give 47p back as two 20 pence coins, three two pence coins
     and one 1 pence coin.
@@ -15,10 +14,12 @@ public abstract class CoinCalculator {
     £2, £1, £0.50, £0.20, £0.10, £0.05, £0.02, £0.01
     Program challenge states that coins that are not given still need to be stated in output i.e.
     "Number of £2 coins is: 0"
+     @param costOfTransaction The amount of money that the total transaction costs for all goods purchased
+     @param moneyTendered The amount of money given to the cashier by a customer
      */
-    public static void calculateChange(BigDecimal anAmount){
+     public static void calculateChange(BigDecimal costOfTransaction, BigDecimal moneyTendered){
         // Variable to keep track of how much change is still required to be given
-        BigDecimal amountLeft = anAmount;
+         BigDecimal amountLeft = new BigDecimal(String.valueOf(moneyTendered.subtract(costOfTransaction)));
 
         // Array holding each number of coin needed (ranging from £2 to 1p)
         BigDecimal[] numOfCoins = {new BigDecimal("0"), new BigDecimal("0"), new BigDecimal("0"),
